4037b966482bfc51b3a551253a43855034ff1e2d
Sabrina Barros <sabrina.barros2045@gmail.com>
2025-06-02 16:00:40 -0300

    fix: lint issues

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'maplibre-gl/dist/maplibre-gl.css'
+import './style.css';
+import App from './App.vue';
+import { createApp } from 'vue';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
